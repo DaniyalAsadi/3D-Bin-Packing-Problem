747e5553c4645206de45839edd0e0a4be5696347
daniyal2224 <daniyal2224@gmail.com>
2025-03-20 14:37:49 +0330

    Update fitness calculation to account for product overlap
    The `CalculateFitness` method in the `FitnessCalculator` class now calculates the fitness value based on the number of overlapping products in a chromosome's placement, instead of returning a constant value.
    
    A new method `OverLappingMatrix` has been added to generate a matrix indicating whether each pair of products overlaps. The `CalculateFitness` method uses this matrix to determine the number of overlapping products and adjusts the fitness value accordingly.

--- 3D Bin Packing Problem/Services/FitnessCalculator.cs
+++ 3D Bin Packing Problem/Services/FitnessCalculator.cs
@@ -1,10 +1,34 @@
 ﻿using _3D_Bin_Packing_Problem.Model;
-
 namespace _3D_Bin_Packing_Problem.Services;
 internal class FitnessCalculator
 {
     public static double CalculateFitness(Chromosome chromosome)
     {
-        return 1000;
+        double fitness = 0;
+        var overlappingMatrix = OverLappingMatrix(chromosome);
+        for (int i = 0; i < chromosome.Placement.PlacedProducts.Count; i++)
+        {
+            for (int j = i + 1; j < chromosome.Placement.PlacedProducts.Count; j++)
+            {
+                if (overlappingMatrix[i, j])
+                {
+                    fitness += 1;
+                }
+            }
+        }
+        return fitness;
+    }
+
+    private static bool[,] OverLappingMatrix(Chromosome chromosome)
+    {
+        bool[,] overlappingMatrix = new bool[chromosome.Placement.PlacedProducts.Count, chromosome.Placement.PlacedProducts.Count];
+        for (int i = 0; i < chromosome.Placement.PlacedProducts.Count; i++)
+        {
+            for (int j = i + 1; j < chromosome.Placement.PlacedProducts.Count; j++)
+            {
+                overlappingMatrix[i, j] = SAT3D.IsColliding(chromosome.Placement.PlacedProducts[i].PositionNodes, chromosome.Placement.PlacedProducts[j].PositionNodes);
+            }
+        }
+        return overlappingMatrix;
     }
 }
