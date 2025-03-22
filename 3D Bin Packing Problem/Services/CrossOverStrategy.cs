966363e8c46a1a246d6ddd8c447914e34832c793
daniyal2224 <daniyal2224@gmail.com>
2025-03-22 18:08:58 +0330

    Refactor CrossOverStrategy and clean up code
    - Made CrossOverStrategy a static class
    - Removed unused using directives
    - Corrected spelling of "Chromosome" in comments
    - Simplified variable declarations with var keyword

--- 3D Bin Packing Problem/Services/CrossOverStrategy.cs
+++ 3D Bin Packing Problem/Services/CrossOverStrategy.cs
@@ -1,13 +1,8 @@
 ﻿using _3D_Bin_Packing_Problem.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _3D_Bin_Packing_Problem.Services;
 
-public class CrossOverStrategy
+public static class CrossOverStrategy
 {
     public static (Chromosome, Chromosome) Crossover(Chromosome parent1, Chromosome parent2)
     {
@@ -20,25 +15,25 @@ public class CrossOverStrategy
         var firstBox = parent1.Placement.Box.Clone();
         var secondBox = parent2.Placement.Box.Clone();
 
-        // Choromosome 1
+        // Chromosome 1
         var firstParts1 = parent1.Placement.PlacedProducts.GetRange(0, crossoverPoint1);
         var secondParts1 = parent2.Placement.PlacedProducts.GetRange(crossoverPoint1, crossoverPoint2 - crossoverPoint1);
         var thirdParts1 = parent1.Placement.PlacedProducts.GetRange(crossoverPoint2, count - crossoverPoint2);
         var pp1 = firstParts1.Concat(secondParts1).Concat(thirdParts1).ToList();
 
-        BoxPlacement firstBoxPlacement = new BoxPlacement(firstBox);
+        var firstBoxPlacement = new BoxPlacement(firstBox);
         firstBoxPlacement.PlaceProduct(pp1);
 
 
 
         var firstChromosome = new Chromosome(firstBoxPlacement);
-        // Choromosome 2
+        // Chromosome 2
         var firstParts2 = parent2.Placement.PlacedProducts.GetRange(0, crossoverPoint1);
         var secondParts2 = parent1.Placement.PlacedProducts.GetRange(crossoverPoint1, crossoverPoint2 - crossoverPoint1);
         var thirdParts2 = parent2.Placement.PlacedProducts.GetRange(crossoverPoint2, count - crossoverPoint2);
         var pp2 = firstParts2.Concat(secondParts2).Concat(thirdParts2).ToList();
 
-        BoxPlacement secondBoxPlacement = new BoxPlacement(firstBox);
+        var secondBoxPlacement = new BoxPlacement(firstBox);
         secondBoxPlacement.PlaceProduct(pp2);
         var secondChromosome = new Chromosome(secondBoxPlacement);
 
